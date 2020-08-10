024a380f1db504095e13df181005ac278ecc6839
terriblebassist <asingh175@gmail.com>
2020-08-11 02:28:04 +0530

    added swagger-ui landing page

--- src/main/java/com/investscape/navgetter/RequestController.java
+++ src/main/java/com/investscape/navgetter/RequestController.java
@@ -1,56 +1,13 @@
 package com.investscape.navgetter;
 
-import com.investscape.navgetter.model.Scheme;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.util.ArrayList;
-
-@RestController
+@Controller
 public class RequestController {
 
-    @Autowired
-    private Environment env;
-
-    private Long lastUpdated;
-
-    private ArrayList<String> mfList;
-
-    private static final String AMFI_WEBSITE_LINK = "https://www.amfiindia.com/spages/NAVAll.txt?t=11012017122537";
-
-    @GetMapping(path = "/getNAV/{schemeCode}")
-    public Scheme getScheme(@PathVariable String schemeCode) throws IOException {
-        ArrayList<String> lines = new ArrayList<>();
-        if (lastUpdated != null
-                && System.currentTimeMillis() - lastUpdated < Long.parseLong(env.getProperty("forceUpdateTimeout"))) {
-            lines = mfList;
-        }
-        else{
-            URL url = new URL(AMFI_WEBSITE_LINK);
-            BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
-            String line;
-            while ((line = in.readLine()) != null) {
-                lines.add(line);
-            }
-            mfList = lines;
-            lastUpdated = System.currentTimeMillis();
-            in.close();
-        }
-
-        for(String mf: lines) {
-            if(mf.startsWith(schemeCode)){
-                String[] elements = mf.split(";");
-                return new Scheme(elements[0], elements[3], elements[4], elements[5]);
-            }
-        }
-        return new Scheme(null,null,null,null);
+    @RequestMapping("/")
+    public String home() {
+        return "redirect:/swagger-ui.html";
     }
-
 }
