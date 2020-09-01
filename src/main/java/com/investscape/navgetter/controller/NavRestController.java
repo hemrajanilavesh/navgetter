b96117fa4a39b9208db593c6056ae658e3382f32
hemrajanilavesh <hemrajanilavesh@gmail.com>
2020-09-01 18:42:07 +0530

    Refactor to load funds by Service

--- src/main/java/com/investscape/navgetter/controller/NavRestController.java
+++ src/main/java/com/investscape/navgetter/controller/NavRestController.java
@@ -1,57 +1,31 @@
 package com.investscape.navgetter.controller;
 
 import com.investscape.navgetter.model.Scheme;
+import com.investscape.navgetter.service.NavService;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.util.ArrayList;
 
 @RestController
 public class NavRestController {
-    private static final String AMFI_WEBSITE_LINK = "https://www.amfiindia.com/spages/NAVAll.txt";
+
     @Autowired
-    private Environment env;
-    private Long lastUpdated;
-    private ArrayList<String> mfList;
+    private NavService navService;
 
     @GetMapping(path = "/getNAV/{schemeCode}")
     @ApiOperation(
             value = "returns NAV",
             notes = "given scheme code of a MF, this API returns the latest NAV present on the AMFI website. For ex: .../getNav/118989"
     )
-    public Scheme getScheme(@PathVariable String schemeCode, @RequestParam(value = "forceUpdate", required = false,
-            defaultValue = "false") boolean forceUpdate) throws IOException {
-        ArrayList<String> lines = new ArrayList<>();
-        if (!forceUpdate && lastUpdated != null
-                && System.currentTimeMillis() - lastUpdated < Long.parseLong(env.getProperty("forceUpdateTimeout"))) {
-            lines = mfList;
-        } else {
-            URL url = new URL(AMFI_WEBSITE_LINK);
-            try (BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()))) {
-                String line;
-                while ((line = in.readLine()) != null) {
-                    lines.add(line);
-                }
-                mfList = lines;
-                lastUpdated = System.currentTimeMillis();
-            }
-        }
+    public Scheme getScheme(@ApiParam(value = "Schema code of a Mutual Fund", defaultValue = "119551") @PathVariable String schemeCode,
+                            @ApiParam(value = "Parameter to force reload of all funds from source.") @RequestParam(value = "forceUpdate", required = false, defaultValue = "false") boolean forceUpdate) throws IOException {
 
-        for (String mf : lines) {
-            String[] elements = mf.split(";");
-            if (elements.length > 0 && elements[0].equals(schemeCode)) {
-                return new Scheme(elements[0], elements[3], elements[4], elements[5]);
-            }
-        }
-        return new Scheme(null, null, null, null);
+        return navService.getNav(forceUpdate, schemeCode);
     }
 }
