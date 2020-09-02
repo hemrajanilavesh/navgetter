14fe65fee21ce736b9d301852670c0ba1c70901b
Abhinav Singh <asingh175@gmail.com>
2020-09-02 22:57:06 +0530

    Revert "Refactor to load funds by Service"
    This reverts commit b96117fa4a39b9208db593c6056ae658e3382f32.

--- src/main/java/com/investscape/navgetter/NavgetterApplication.java
+++ src/main/java/com/investscape/navgetter/NavgetterApplication.java
@@ -1,29 +1,13 @@
 package com.investscape.navgetter;
 
-import com.investscape.navgetter.service.NavService;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
 
-import java.io.IOException;
-
-@Slf4j
 @SpringBootApplication
 public class NavgetterApplication {
 
-	@Autowired
-	private NavService navService;
-
 	public static void main(String[] args) {
 		SpringApplication.run(NavgetterApplication.class, args);
-	}
-
-	@EventListener(ApplicationReadyEvent.class)
-	public void loadAllFunds() throws IOException {
-		navService.loadNavForAllFunds();
 	}
 
 }
