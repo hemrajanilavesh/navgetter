b4fbe644b42f06bf69f17812daf21a1a0d7f3c9f
Abhinav Singh <asingh175@gmail.com>
2020-09-02 22:57:06 +0530

    Revert "Better logging"
    This reverts commit 3d1849dfdd78989a149f8660062f124d2330f3d1.

--- src/main/java/com/investscape/navgetter/CacheEventLogger.java
+++ src/main/java/com/investscape/navgetter/CacheEventLogger.java
@@ -3,21 +3,12 @@ package com.investscape.navgetter;
 import lombok.extern.slf4j.Slf4j;
 import org.ehcache.event.CacheEvent;
 import org.ehcache.event.CacheEventListener;
-import org.ehcache.event.EventType;
-
-import java.util.Arrays;
 
 @Slf4j
 public class CacheEventLogger implements CacheEventListener<Object, Object> {
 
     @Override
     public void onEvent(CacheEvent<?, ?> cacheEvent) {
-        if (Arrays.asList(EventType.EXPIRED, EventType.EVICTED, EventType.REMOVED).contains(cacheEvent.getType())) {
-            log.info(cacheEvent.getType().name() + " : Value: {}", cacheEvent.getOldValue());
-        } else if (Arrays.asList(EventType.UPDATED).contains(cacheEvent.getType())) {
-            log.info(cacheEvent.getType().name() + " : Old Value: {}, New Value: {}", cacheEvent.getOldValue(), cacheEvent.getNewValue());
-        } else {
-            log.info(cacheEvent.getType().name() + " : Value: {}", cacheEvent.getNewValue());
-        }
+        log.info(cacheEvent.getType().name() + " : Value: {}", cacheEvent.getNewValue());
     }
 }
--- src/main/java/com/investscape/navgetter/model/Scheme.java
+++ src/main/java/com/investscape/navgetter/model/Scheme.java
@@ -3,7 +3,6 @@ package com.investscape.navgetter.model;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.Serializable;
@@ -11,7 +10,6 @@ import java.io.Serializable;
 @Slf4j
 @Setter
 @Getter
-@ToString
 @AllArgsConstructor
 public class Scheme implements Serializable {
     String schemeCode;
