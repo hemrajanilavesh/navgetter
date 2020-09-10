74604ef36760d53b7bf0f041144edbdd4ff0b8b0
Lavesh Hemrajani <hemrajanilavesh@gmail.com>
2020-09-10 11:32:41 +0530

    API Model Swagger documentation (#8)
    * Adding Swagger doc to Api Model
    
    * Commit for circle ci

--- src/main/java/com/investscape/navgetter/model/Scheme.java
+++ src/main/java/com/investscape/navgetter/model/Scheme.java
@@ -1,5 +1,7 @@
 package com.investscape.navgetter.model;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,14 +10,23 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.io.Serializable;
 
+@ApiModel(value = "Scheme", description = "Details of Mutual Fund Scheme including the Net Asset Value (NAV)")
 @Slf4j
 @Setter
 @Getter
 @ToString
 @AllArgsConstructor
 public class Scheme implements Serializable {
+
+    @ApiModelProperty(value = "Uniquely identifiable code of the Mutual Fund Scheme.", example = "119551")
     String schemeCode;
+
+    @ApiModelProperty(value = "Name of the Mutual Fund Scheme.", example = "Aditya Birla Sun Life Banking & PSU Debt Fund - Direct Plan-Dividend")
     String schemeName;
+
+    @ApiModelProperty(value = "The Net Asset Value of the Mutual Fund Scheme, reported on the corresponding date to AMFI.", example = "156.2123")
     String nav;
+
+    @ApiModelProperty(value = "Date corresponding to the Net Asset Value.", example = "09-Sep-2020")
     String date;
 }
